8ee777f816b77e699762e748c7087bb094669ecc
umar-masood <umarmasood8546@gmail.com>
2025-12-27 19:59:55 +0500

    Issue Fixed: Not gain focus when it is visually shown

--- src/ui/accountOTP/OTPWidget.cpp
+++ src/ui/accountOTP/OTPWidget.cpp
@@ -1,7 +1,6 @@
 #include "OTPWidget.h"
 
-OTPWidget::OTPWidget(QWidget *parent) : QWidget(parent)
-{
+OTPWidget::OTPWidget(QWidget *parent) : QWidget(parent) {
    setAttribute(Qt::WA_TranslucentBackground);
    setFixedSize(QSize(360, 60));
 
@@ -14,8 +13,7 @@ OTPWidget::OTPWidget(QWidget *parent) : QWidget(parent)
    font.setBold(true);
    font.setPointSize(22);
 
-   for (int i = 0; i < boxes; i++)
-   {
+   for (int i = 0; i < boxes; i++) {
       auto *box = new QLabel;
       box->setFont(font);
       box->setAlignment(Qt::AlignCenter);
@@ -35,22 +33,19 @@ OTPWidget::OTPWidget(QWidget *parent) : QWidget(parent)
    updateHighlight();
 }
 
-void OTPWidget::setDarkMode(bool value)
-{
+void OTPWidget::setDarkMode(bool value) {
    isDarkMode = value;
    updateStyles();
    updateHighlight();
 }
 
-void OTPWidget::keyPressEvent(QKeyEvent *event)
-{
+void OTPWidget::keyPressEvent(QKeyEvent *event) {
    const int n = OTPBoxes.size();
    if ((event->key() >= Qt::Key_0 && event->key() <= Qt::Key_9) ||
       (event->key() >= Qt::Key_A && event->key() <= Qt::Key_Z))
    {
       QString text = event->text().toUpper();
-      if (!text.isEmpty() && currentIndex >= 0 && currentIndex < n)
-      {
+      if (!text.isEmpty() && currentIndex >= 0 && currentIndex < n) {
          OTPBoxes[currentIndex]->setText(text);
          currentIndex++;
          if (currentIndex >= n)
@@ -58,24 +53,18 @@ void OTPWidget::keyPressEvent(QKeyEvent *event)
          updateHighlight();
       }
 
-      if (currentIndex == n - 1 && !OTPBoxes[n - 1]->text().isEmpty())
-      {
+      if (currentIndex == n - 1 && !OTPBoxes[n - 1]->text().isEmpty()) {
          QString c;
          for (auto *i : OTPBoxes)
             c.append(i->text());
          emit OTPcompleted(c);
       }
-   }
-   else if (event->key() == Qt::Key_Backspace)
-   {
-      if (currentIndex >= 0 && currentIndex < n)
-      {
-         if (!OTPBoxes[currentIndex]->text().isEmpty())
-         {
+
+   } else if (event->key() == Qt::Key_Backspace) {
+      if (currentIndex >= 0 && currentIndex < n) {
+         if (!OTPBoxes[currentIndex]->text().isEmpty()) {
             OTPBoxes[currentIndex]->clear();
-         }
-         else
-         {
+         } else {
             if (currentIndex > 0)
                currentIndex--;
             OTPBoxes[currentIndex]->clear();
@@ -85,8 +74,7 @@ void OTPWidget::keyPressEvent(QKeyEvent *event)
    }
 }
 
-void OTPWidget::updateStyles()
-{
+void OTPWidget::updateStyles() {
    QString bg_color_normal = isDarkMode ? "#2D2D2D" : "#FBFBFB";
    QString bg_color_focused = isDarkMode ? "#1F1F1F" : "#FFFFFF";
    QString text_color = isDarkMode ? "white" : "black";
@@ -109,10 +97,8 @@ void OTPWidget::updateStyles()
                      .arg(border_color);
 }
 
-void OTPWidget::updateHighlight()
-{
-   for (int i = 0; i < OTPBoxes.size(); i++)
-   {
+void OTPWidget::updateHighlight() {
+   for (int i = 0; i < OTPBoxes.size(); i++) {
       if (!isEnabled()) 
          OTPBoxes[i]->setStyleSheet(disabledStyle);
       else if (i == currentIndex)
@@ -126,4 +112,9 @@ void OTPWidget::setEnabled(bool enabled) {
    QWidget::setEnabled(enabled);
    updateStyles();
    updateHighlight();
+}
+
+void OTPWidget::showEvent(QShowEvent *event) {
+   QWidget::showEvent(event);
+   this->setFocus(Qt::OtherFocusReason);
 }
