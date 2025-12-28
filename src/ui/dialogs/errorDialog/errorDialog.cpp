f9d9dfa4125d7114235668c09b48a533145e6ce5
umar-masood <umarmasood8546@gmail.com>
2025-12-28 09:27:31 +0500

    Make a reusable error dialog to avoid redundant creation of dialogs

---
+++ src/ui/dialogs/errorDialog/errorDialog.cpp
@@ -0,0 +1,109 @@
+#include "errorDialog.h"
+
+Error::Error(QSize widgetSize, const QString &text, const QString &illustrationLight, const QString &illustrationDark, QSize iconSize, QWidget *parent) : QWidget(parent), lightIcon(illustrationLight), darkIcon(illustrationDark), size(iconSize) {
+    
+    setAttribute(Qt::WA_TranslucentBackground);
+    setFixedSize(widgetSize);
+
+    layout = new QVBoxLayout(this);
+    layout->setSpacing(0);
+    //layout->setContentsMargins(10, 16, 10, 10);
+
+    // Action Button
+    actionBtn = new Button;
+    actionBtn->setDisplayMode(Button::TextOnly);
+    actionBtn->setFixedSize(QSize(316, 36));
+    actionBtn->setText("Retry");
+
+    // Icon
+    illustration = new QLabel;
+    illustration->setAttribute(Qt::WA_TranslucentBackground);
+    illustration->setFixedSize(size);
+    illustration->setAlignment(Qt::AlignCenter);
+    illustration->setPixmap(QPixmap(lightIcon).scaled(size, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+
+    // Text (only if provided)
+    if (!text.isEmpty()) {
+        textWidget = new Text;
+        textWidget->setFixedSize(QSize(290, 36));
+        textWidget->setFont("Segoe UI", 10, false, false, Qt::AlignCenter);
+        textWidget->setTextColor("#8D8D8D");
+        textWidget->setWordWrap(true);
+        textWidget->setText(text);
+    }
+
+    layout->addStretch();
+    layout->addSpacing(4);
+    layout->addWidget(illustration, 0, Qt::AlignCenter);
+    layout->addSpacing(16);
+
+    if (textWidget) {
+        layout->addWidget(textWidget, 0, Qt::AlignCenter);
+        layout->addSpacing(16);
+    }
+
+    layout->addWidget(actionBtn, 0, Qt::AlignCenter);
+    layout->addStretch();
+
+    // Theme Change Signal Slot
+    connect(this, &Error::themeModeChanged, this, &Error::onThemeModeChanged);
+}
+
+void Error::onThemeModeChanged(bool enable) {
+    if (illustration)
+        illustration->setPixmap(QPixmap(enable ? darkIcon : lightIcon).scaled(size, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+}
+
+void Error::setDarkMode(bool value) {
+    if (isDarkMode == value) return;
+    isDarkMode = value;
+    emit themeModeChanged(isDarkMode); 
+}
+
+Button * Error::actionButton() { return actionBtn; }
+
+/* ---------------  Error Dialog Manager  --------------- */
+ErrorDialogManager::ErrorDialogManager(AccountWindow *window, QObject *parent) : QObject(parent), accountWindow(window) {
+
+    if (!accountWindow) return;
+
+    create("InvalidCredentials", wrongCredentialsText, wrongCredentialsIcon);
+    create("MaxAttempts", maxAttemptsText, maxAttemptsIcon);
+    create("SomethingWentWrong", somethingWentWrongText, somethingWentWrongIcon);
+    create("AccessDenied", accessDeniedText, accessDeniedIcon);
+    create("RequestTimeout", timeoutText, timeoutIcon);
+
+    accountWindow->setSubWidgets(allWidgets());
+}
+
+void ErrorDialogManager::create( const QString &key, const QString &text, const QString &iconPath) {
+    ErrorDialog ed;
+    ed.widget = new Error(dialogSize, text, iconPath, iconPath, illustrationSize);
+    ed.dialog = new Dialog(ed.widget, accountWindow->subWindow(), false);
+
+    connect(ed.widget->actionButton(), &Button::clicked, this, [this, key]() {
+        emit actionTriggered(key);
+        close(key);
+    });
+
+    dialogs.insert(key, ed);
+}
+
+void ErrorDialogManager::show(const QString &key) {
+    if (dialogs.contains(key) && dialogs[key].dialog) 
+        dialogs[key].dialog->show();
+    
+}
+
+void ErrorDialogManager::close(const QString &key) {
+    if (dialogs.contains(key) && dialogs[key].dialog) 
+        dialogs[key].dialog->close();
+}
+
+QList<QWidget *> ErrorDialogManager::allWidgets() const {
+    QList<QWidget *> widgets;
+    for (const auto &d : dialogs) 
+        widgets << d.widget << d.dialog;
+    
+    return widgets;
+}
---
+++ src/ui/dialogs/errorDialog/errorDialog.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "../../components/Dialog.h"
+#include "../dialogsUtils.h"
+#include "../../accountWindow/AccountWindow.h"
+#include "../../windows/subWindow/SubWindow.h"
+#include "../../components/Button.h"
+#include "../../components/Text.h"
+
+#include <QObject>
+#include <QMap>
+#include <QList>
+
+using namespace DialogUtils;
+
+class Error : public QWidget {
+    Q_OBJECT
+
+    public:
+    explicit Error ( QSize widgetSize = QSize(300,300),
+                    const QString &text = QString(),
+                    const QString &illustrationLight = QString(),
+                    const QString &illustrationDark = QString(),
+                    QSize iconSize = QSize(203, 201),
+                    QWidget *parent = nullptr);
+
+    Q_INVOKABLE void setDarkMode(bool value);
+    Button* actionButton();
+
+    signals:
+    void themeModeChanged(bool enable);
+
+    private slots:
+    void onThemeModeChanged(bool enable);
+
+    private:
+    bool isDarkMode = false;
+
+    Text *textWidget = nullptr;
+    Button *actionBtn = nullptr;
+    QLabel *illustration = nullptr;
+    QVBoxLayout *layout = nullptr;
+
+    QString lightIcon;
+    QString darkIcon;
+    QSize size;
+};
+
+
+class ErrorDialogManager : public QObject {
+    Q_OBJECT
+
+    public:
+    explicit ErrorDialogManager(AccountWindow *window, QObject *parent = nullptr);
+
+    void show(const QString &key);
+    void close(const QString &key);
+    QList<QWidget *> allWidgets() const;
+
+    signals:
+    void actionTriggered(const QString &key);
+
+    private:
+    struct ErrorDialog {
+        Error *widget = nullptr;
+        Dialog *dialog = nullptr;
+    };
+
+    AccountWindow *accountWindow = nullptr;
+    QMap<QString, ErrorDialog> dialogs;
+
+    void create(
+        const QString &key,
+        const QString &text,
+        const QString &iconPath
+    );
+};
