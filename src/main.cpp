8b00b3558553ce214db8a7e9d9071dbc9aa17824
umar-masood <umarmasood8546@gmail.com>
2026-01-02 19:48:57 +0500

    Updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,28 @@
 #include "ui/accountWindow/AccountWindow.h"
-#include "./core/accountSignInManager/accountSignInManager.h"
 #include "./ui/accountSignIn/AccountSignIn.h"
 #include "./ui/accountOTP/AccountOTP.h"
+#include "./ui/accountCreate/AccountCreate.h"
 #include "./core/validators/OTPValidator/OTPValidator.h"
-
+#include "./core/deviceInfo/deviceInfo.h"
+#include "./core/accountSignInManager/accountSignInManager.h"
+#include "./core/accountCreationManager/accountCreationManager.h"
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    QCoreApplication::setApplicationName("Vaultorix");
+    QCoreApplication::setApplicationVersion("1.0.0");
+    QCoreApplication::setOrganizationName("Umar Creations");
     
     AccountWindow *aw = new AccountWindow;
-    AccountSignIn *ac = new AccountSignIn;
+
+    AccountCreate *ac = new AccountCreate(nullptr, aw);
     aw->setRightWidget(ac);
 
-    AccountSignInManager *asmgr = new AccountSignInManager(aw);
-    asmgr->setAccountSignInObject(ac);
+    AccountCreationManager *m = new AccountCreationManager(aw);
+    m->setAccountCreateObject(ac);
 
 
     aw->show();
     return app.exec();
 }
+
