e49db51446b470077db1db9e2e0a344ee26625ab
umar-masood <umarmasood8546@gmail.com>
2026-08-17 09:17:59 +0500

    Add clear() before loading new domains into data structures.

--- src/core/validators/auth/PasswordValidator.cpp
+++ src/core/validators/auth/PasswordValidator.cpp
@@ -2,69 +2,54 @@
 #include "PasswordValidator.h"
 #include "../../config/APIConfig.h"
 #include "../../config/Constants.h"
-#include "../../utils/Utils.h"
-#include <QDebug>
+
 #include <QUrl>
-#include <fstream>
+#include <QFile>
+#include <QTextStream>
 
 PasswordValidator::PasswordValidator(QObject *parent) : QObject(parent) {
-    blacklistManager = new Utils::BlacklistManager(this, "Password");
-    blacklistManager->setFileName(PWD_BLACKLIST_FILE);
+    _blacklistManager = new Utils::BlacklistManager("Password", this);
+    _blacklistManager->setFileName(PWD_BLACKLIST_FILE);
 
-    if (blacklistManager->downloadList(QUrl(QString::fromUtf8(APIRoutes::PWD_BLACKLIST))))
-        INFO_HERE("Password blacklist is being downloaded.");
+    if (_blacklistManager->downloadList(QUrl(QString::fromUtf8(APIRoutes::PWD_BLACKLIST))))
+        INFO_HERE("Password blacklist download started.");
     else
-        loadPwdsFromFile();
+        loadBlacklistedPwdsFromFile();
 
-    connect(blacklistManager, &Utils::BlacklistManager::listDownloaded, this, [this]() {
+    connect(_blacklistManager, &Utils::BlacklistManager::listDownloaded, this, [this]() {
         INFO_HERE("Password blacklist has been downloaded.");
-        loadPwdsFromFile();
+        loadBlacklistedPwdsFromFile();
     });
 }
 
 void PasswordValidator::checkPasswordValidity(const QString &password) {
     PasswordValidationResult result;
 
-    result.hasLength = password.size() >= 8;
-
-    bool hasUpper = false;
-    bool hasLower = false;
-    bool hasDigit = false;
-    bool hasSpecial = false;
-
     for (QChar ch : password) {
         ushort c = ch.unicode();
 
         if (c >= 'A' && c <= 'Z')
-            hasUpper = true;
+            result.hasUpper = true;
         else if (c >= 'a' && c <= 'z')
-            hasLower = true;
+            result.hasLower = true;
         else if (c >= '0' && c <= '9')
-            hasDigit = true;
+            result.hasDigit = true;
         else
-            hasSpecial = true;
+            result.hasSpecial = true;
     }   
 
-    result.hasUpper = hasUpper;
-    result.hasLower = hasLower;
-    result.hasDigit = hasDigit;
-    result.hasSpecial = hasSpecial;
-
-    QString lowerPwd = password.toLower();
-    std::string pwdStd = lowerPwd.toStdString();
-
-    if (weakPwds.empty()) {
+    if (_blacklistedPasswords.empty()) {
         result.notWeak = false;
         result.isStrong = false;
 
         emit validationUpdated(result);
 
-        ERROR_HERE("Weak passwords list is not loaded.");
+        ERROR_HERE("Weak passwords list was not loaded.");
         return;
     }
 
-    result.notWeak = !isPasswordBlacklisted(pwdStd);
-
+    result.hasLength = password.size() >= 8;
+    result.notWeak = !isPasswordBlacklisted(password);
     result.isStrong = result.hasLength  &&
                       result.hasUpper   &&
                       result.hasLower   &&
@@ -75,51 +60,49 @@ void PasswordValidator::checkPasswordValidity(const QString &password) {
     emit validationUpdated(result);
 }
 
-bool PasswordValidator::isPasswordBlacklisted(const std::string &password) {
-    auto it = cacheMap.find(password);
-
-    if (it != cacheMap.end()) {
-        order.splice(order.end(), order, it->second);
+bool PasswordValidator::isPasswordBlacklisted(const QString &password) {
+    auto it = _cacheMap.find(password);
+    if (it != _cacheMap.end()) {
+        _order.splice(_order.end(), _order, it->second);
         return true;
     }
 
-    bool result = weakPwds.find(password) != weakPwds.end();
-
+    bool result = _blacklistedPasswords.find(password) != _blacklistedPasswords.end();
     if (result) {
-        order.push_back(password);
-        cacheMap[password] = std::prev(order.end());
-        if (cacheMap.size() > MAX_CACHE_SIZE) {
-            std::string oldestPwd = order.front();
-            cacheMap.erase(oldestPwd);
-            order.pop_front();
+        _order.push_back(password);
+        _cacheMap[password] = std::prev(_order.end());
+
+        if (_cacheMap.size() > MAX_CACHE_SIZE) {
+            QString oldestPwd = _order.front();
+            _cacheMap.erase(oldestPwd);
+            _order.pop_front();
         }
     }
 
     return result;
 }
 
-void PasswordValidator::loadPwdsFromFile() {
-    std::ifstream file(blacklistManager->filePath);
-
-    if (!file.is_open()) {
-        ERROR_HERE("Could not open file.");
+void PasswordValidator::loadBlacklistedPwdsFromFile() {
+    QFile file(_blacklistManager->filePath());
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        ERROR_HERE("Weak passwords list failed to open.");
         return;
     }
 
-    weakPwds.reserve(131554);
+    _blacklistedPasswords.clear();
+    _blacklistedPasswords.reserve(131554);
 
-    std::string line;
-    while (std::getline(file, line)) {
-        if (!line.empty() && (line.back() == '\r' || line.back() == '\n'))
-            line.pop_back();
-
-        Utils::lower(line);
-
-        if (!line.empty())
-            weakPwds.insert(line);
+    _order.clear();
+    _cacheMap.clear();
+    
+    QTextStream stream(&file);
+    while (!stream.atEnd()) {
+        QString password = stream.readLine().trimmed();
+        if (!password.isEmpty())
+            _blacklistedPasswords.insert(password);
     }
 
     file.close();
 
-    INFO_HERE("Loaded " + QString::number(weakPwds.size()) + " passwords.");
+    INFO_HERE("Loaded " + QString::number(_blacklistedPasswords.size()) + " passwords.");
 }
